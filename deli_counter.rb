bc2259624c9b69750374033e4b682f9cb5d60051
Meisuleen Kased <meisuleen@gmail.com>
2018-11-10 02:29:27 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+require 'pry'
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+
+    #  katz_deli.each.with_index {|person, index| person}
+
+       katz_deli.each do |person|
+         puts "The line is currently:"
+         puts "1. #{person} 2. #{person} 3. #{person}"
+      end
+  end
+end
+
+#["Logan", "Avi", "Spencer"]
