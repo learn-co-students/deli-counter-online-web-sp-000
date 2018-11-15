f12f01296f2e62216aac8c6d18065062a95c7294
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-15 00:31:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+    return
+  end
+
+    string = "The line is currently:"
+    katz_deli.each  {|customer|string << " #{katz_deli.index(customer) + 1}. #{customer}"}
+    puts string
+
+  end
+
+def take_a_number(katz_deli, person)
+  katz_deli.push(person)
+    puts "Welcome, #{person}. You are number #{katz_deli.index(person) + 1} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  end
+ while katz_deli.length > 0
+   puts "Currently serving #{katz_deli[0]}."
+   katz_deli.shift
+ end
+
+end
