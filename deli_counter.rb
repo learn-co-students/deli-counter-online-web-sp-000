f0cf77b9e9dc71648f341b37ab42b2c7adef88d9
Diana Badas <diana.badas@gmail.com>
2018-12-20 16:12:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,14 @@
 # Write your code here.
 katz_deli = []
 
-def line (katz_deli)
-  if katz_deli.length == 0
+def line (other_deli)
+  if other_deli.length == 0
     puts "The line is currently empty."
+  else
+    arr = ""
+    other_deli.each_with_index do |name, index|
+      arr << "#{index + 1}. #{name} "
+    end
+    puts "The line is currently: " + arr.rstrip
   end
 end
