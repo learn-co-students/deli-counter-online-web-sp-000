d3488c16344b3ac33054dd1a142e65f005e9fc1c
Corey H <c.l.halicke@gmail.com>
2019-02-12 04:51:59 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,15 @@
 katz_deli = []
 
 def line(katz_deli)
-  counter = 1 
   if katz_deli.length == 0 
     puts "The line is currently empty."
   elsif
-    
+    counter = 1
+    positions = []
+    katz_deli.each do | name |
+      positions.push("#{counter}. #{name}")
+      counter += 1
+    end
+    puts "The line is currently: #{}"
   end
 end
