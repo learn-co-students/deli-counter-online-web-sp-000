b342e4c45c47fb89fdc25caf613b3566328c1bef
Kimberly Rouse <ksrouse94@gmail.com>
2019-02-20 00:01:07 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(array)
+  if array.count > 0 
+    count = 1
+    array.each { |name|
+      puts "The line is currently: #{count}. #{name} "
+      count++
+    
+  end  
+end  
