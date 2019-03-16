302ec146cf96d4c5d6efc121c319bdfb4d23cf89
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-03-16 21:47:13 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,7 @@
-# Write your code here.
+def line(katz_deli)
+  puts("The line is currently empty.")
+end
+  
+  def take_a_number(katzDeli, name) 
+  katzDeli.push(`${name}`);
+  return(`Welcome, ${name}. You are number ${katzDeli.length} in line.`)
