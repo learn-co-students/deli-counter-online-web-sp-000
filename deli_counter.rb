11d069f85374c5fc77ad2ba5f879775cc8df3bdd
Lauren McCoy <lmccoy8911@gmail.com>
2019-03-18 16:43:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+def line(deli)
+  
+  if deli == []
+    puts "The line is currently empty."
+  else
+    new_array = deli.each_with_index.map {|line, index| "#{index + 1}. #{line}"} 
+    puts "The line is currently: #{new_array.join(" ")}"
+  end
+end 
