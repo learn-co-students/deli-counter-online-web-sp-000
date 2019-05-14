05e2e6167345ae91022a9e3cac11bc490e41904a
Heeraj Ramsanai <ramsanai_heeraj@live.com>
2019-05-14 01:13:29 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    new_line = []
+    katz_deli.each_with_index do |customers,index|
+      new_line << "index. customers"
+    end
+    new_line.join(",")
+    puts"The line is currently: #{customers}"
+    end
+  end
+end
