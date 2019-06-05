555ada161c9391366daf9e8acb862a9b1f7e81a0
Connor Cowan <ccowan003@comcast.net>
2019-06-05 23:39:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+def line(katz_deli)
+    message = "The line is currently"
+    if katz_deli == 0
+      puts "#{message} empty"
+    else
+      katz_deli.each_with_index
+
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+
+end
+
+def now_serving(katz_deli)
+  if katz_deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli.shift}"
+
+end
