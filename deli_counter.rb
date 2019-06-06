5bb9ebedeae83775dbd6dbcdfd342996ebca4494
Connor Cowan <ccowan003@comcast.net>
2019-06-06 00:36:36 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,22 +1,27 @@
-def line(katz_deli)
-    message = "The line is currently"
-    if katz_deli == 0
-      puts "#{message} empty"
+#Enumeration refers to traversing over objects. In Ruby, we call an object enumerable when it describes a set of items and a method to loop over each of them.
+
+def line(deli)
+    if deli.empty?                                        #.empty? method
+      puts "The line is currently empty."
     else
-      katz_deli.each_with_index
-
+      message = "The line is currently:"
+      deli.each.with_index(1) { |name, int|       #enumerator (chained together) with_index takes optional parameter of starting index (1). which is starting point in line
+        message << " #{int}. #{name}"                     #interpolating 'int' to number in line and 'name' to person
+      }
+      puts message
+    end
 end
 
-def take_a_number(katz_deli, name)
-  katz_deli << name
-    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-
+def take_a_number(deli, name)
+  deli << name                                                        #add name to array
+    puts "Welcome, #{name}. You are number #{deli.length} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli.length == 0
+def now_serving(deli)
+  if deli.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{katz_deli.shift}"
-
+    puts "Currently serving #{deli.first}."     #interpolating the current first element (name) in array to be called out
+    deli.shift                                  #removes that element now that they have been served
+  end
 end
