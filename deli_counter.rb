b99eb8c13e56263b6c8d7eaaa033951161dd1fa4
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-04 23:16:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+katz_deli=[]
+katz_deli=[]
+def line(katz_deli)
+   if katz_deli.empty?
+     puts "The line is currently empty."
+   else
+    in_line = ""
+     katz_deli.each_with_index do |name, index|
+       in_line += " #{index +1}. #{name}"
+      end
+       puts "The line is currently:#{in_line}"
+     end
+end
+
+def take_a_number(katz_deli, name)
+ katz_deli.push(name)
+ more_in_line = "Welcome, #{name}. You are number #{katz_deli.length} in line."
+ puts "#{more_in_line}"
+ end
+#   more_in_line_2 = "#{more_in_line} += "
+# more_in_line += " Welcome, #{name}. You are number #{index + 1} in line."
