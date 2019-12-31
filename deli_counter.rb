1b95c19e0f8c90fd6c0d8b80facad6f779d83580
Doak Covington <doakcovington@protonmail.com>
2019-12-31 04:15:26 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+#line method
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    sentence = []
+    line_count = 1
+    array.each do |name|
+      sentence << "#{line_count}. #{name}"
+      line_count += 1
+    end
+    puts "The line is currently: #{sentence.join(" ")}"
+  end
+end #end of line method
+
+#take_a_number method
+def take_a_number(array,name)
+  array << "#{name}"
+  puts "Welcome, #{name}. You are number #{array.length} in line."
+end #end of take_a_number
+
+#now_serving method
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array.first}."
+    array.shift
+  end
+
+end #end of now_serving
