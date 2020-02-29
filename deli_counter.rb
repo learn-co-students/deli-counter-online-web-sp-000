6e7c9ca63759c2d7c3ee58b7bda5d1af76506d66
Alex Romo III <alexromo3rd@gmail.com>
2020-02-29 14:07:30 -0800

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,5 @@
 def line(katz_deli)
-  if katz_deli.size == 0
+  if katz_deli.empty?
     puts "The line is currently empty." 
   else 
     line = []
@@ -16,7 +16,7 @@ def take_a_number(katz_deli, name)
 end
 
 def now_serving(katz_deli)
-  if katz_deli.length == 0
+  if katz_deli.empty?
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli.first}." 
