f57231a7c93f0748cd38d428982e9ba0dfc8a1b3
C. Leow <chienleow1@gmail.com>
2020-03-03 01:58:19 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,13 +3,15 @@ require 'pry'
 def line(katz_deli)
   ## binding.pry
   if katz_deli = []
-   then puts "The line is currently empty."
+   puts "The line is currently empty."
   elsif line(other_deli)
-    then other_deli.each_with_index.map do |name, x|
+    other_deli.each_with_index.map do |name, x|
       wait_number = x + 1
       "The line is currently: #{wait_number}. #{other_deli}"
   else line(another_deli)
-    then
+    another_deli.each_with_index.map do |name, y|
+      wait_number = y + 1
+      "The line is currently: #{wait_number}. #{another_deli}"
   end
  end
 end
@@ -17,4 +19,5 @@ end
 def take_a_number(katz_deli)
   if katz_deli = []
     then 
+  end
 end
