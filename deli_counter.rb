ae70d6d2abe920fc26972cac63fcb320eab2cacc
C. Leow <chienleow1@gmail.com>
2020-03-03 04:17:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,16 +3,16 @@ def line(katz_deli)
    puts "The line is currently empty."
   else
     string = "The line is currently:"
-      katz_deli.each_with_index.map do |name, x|
-      wait_number = x + 1
-      string << "#{wait_number}. #{name}"
-    end
+    katz_deli.each_with_index.map do |name, x|
+    wait_number = x + 1
+    string << " #{wait_number}. #{name}"
+  end
     puts string
   end
 end
 
 def take_a_number(katz_deli)
-  if katz_deli = []
+  if katz_deli == []
     then 
   end
 end
