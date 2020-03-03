ba3134a7843b64d45feba18ff336cc4d1b9eb6a8
C. Leow <chienleow1@gmail.com>
2020-03-03 01:44:32 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+require 'pry'
+
+def line(katz_deli)
+  ## binding.pry
+  if katz_deli = []
+   then puts "The line is currently empty."
+  else
+    other_deli.each_with_index.map do |name, x|
+      wait_number = x + 1
+      "The line is currently: #{wait_number}. #{other_deli}"
+    end
+  end
+end
