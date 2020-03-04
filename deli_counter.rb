ca6f812e05635265b46cff669ffb8e8c47732be5
C. Leow <chienleow1@gmail.com>
2020-03-04 04:31:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def line(katz_deli)
   if katz_deli == []
    puts "The line is currently empty."
@@ -16,11 +17,12 @@ def take_a_number(katz_deli,name)
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving(katz_deli)
-  if katz_deli = []
+def now_serving(other_deli)
+  ## binding.pry
+  if other_deli == []
     puts "There is nobody waiting to be served!"
   else
-    name = katz_deli.shift
-
+    puts "Currently serving #{other_deli[0]}."
+    name = other_deli.shift
   end
 end
