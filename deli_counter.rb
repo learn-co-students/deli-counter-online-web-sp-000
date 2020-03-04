b649723fb98225889ff9332f4f8dee86f6d9472f
C. Leow <chienleow1@gmail.com>
2020-03-04 03:03:24 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,13 +12,15 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli,name)
-  ## the array for the current line of people (katz_deli)
-  ## a string containing the name of the person joining the end of the line
-  ## call out (puts) the person's name along with their position in line
-  ## Welcome, Ada. You are number 1 in line.
-  
-  katz_deli << name
-  puts 
+  if katz_deli == []
+   katz_deli << #{name}
+  else
+    string = "The line is currently:"
+    katz_deli.each_with_index.map do |name, x|
+    wait_number = x + 1
+    string << " #{wait_number}. #{name}"
+  end
+    puts string
+  end
 end
-
 
