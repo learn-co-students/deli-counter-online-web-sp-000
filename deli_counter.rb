f59c79fe26663652834280ec90bb47f75346b27f
Sonja Parsell <hello@sparsell.com>
2020-03-25 16:32:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
-# Write your code here.
+
+katz_deli = ["Logan", "Avi", "Spencer", "Harry", "Charles", "Amber", "Scarlett"]
+
+def line(katz_deli)
+     if katz_deli.length == 0
+       puts "The line is currently empty."
+     else
+      katz_deli.each_with_index do |customer, index|
+       puts "Welcome #{customer}. You are number #{index + 1} in line."
+      end
+     end
+end
+
+def take_a_number(katz_deli, customer)
+  katz_deli.each_with_index do |customer, index|
+  puts "Welcome #{customer}. You are number #{index + 1} in line."
+end
+end
+
+def now_serving(katz_deli)
+
+end
