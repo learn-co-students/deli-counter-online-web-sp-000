820ecfc291e5aac32c39ee880eb112402d301ab8
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-13 19:22:16 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    string = ""
+    array.each do |name|
+      string << " #{array.index(name)+1}. #{name}"
+    end
+    puts "The line is currently:#{string}"
+  end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{name}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
+
