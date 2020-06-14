5bfeef3ec23196686f152016bbe69882066ab6b1
reevesalic <reevesalic@gmail.com>
2020-06-14 18:37:53 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,21 +1,19 @@
-# Write your code here.
 katz_deli = []
 
-def line(katz_deli)
-     if katz_deli.length == 0
-        puts "The line is empty."
-     else
-        puts "The is currently: #{other_deli}"
+def line(length_of_line)
+  
+  if length_of_line.size == 0
+    puts "The line is currently empty."
+  else
+    message = "The line is currently: "
+    length_of_line.each_with_index do |name, index|
+      message += "#{index + 1}. #{name} "
+    end
+    puts message.rstrip
   end
-       #return line(other_deli)
-end
-     
-
-def take_a_number(katz_deli, other_deli)
-     katz_deli << other_deli
-     #puts take_a_number(katz_deli, other_deli)
-     puts "Welcome, #{other_deli}. You are #{katz_deli.length} in line"
-     
-     
 end
 
+def take_a_number(length_of_line, name)
+  length_of_line << name
+  puts "Welcome, #{name}. You are number #{length_of_line.size} in line."
+end
