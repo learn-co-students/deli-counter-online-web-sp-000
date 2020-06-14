9bdca90d1bccd7b64eb73a9093ac36ae90de35dd
reevesalic <reevesalic@gmail.com>
2020-06-14 16:19:52 -0400

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,20 +1,21 @@
 # Write your code here.
 katz_deli = []
 
-def line(katz_deli, name)
+def line(katz_deli)
      if katz_deli.length == 0
-          puts "The line is empty."
+        puts "The line is empty."
      else
-          puts "The is currently:"
+        puts "The is currently: #{other_deli}"
   end
        #return line(other_deli)
 end
      
 
 def take_a_number(katz_deli, other_deli)
-     katz_deli << name
+     katz_deli << other_deli
+     #puts take_a_number(katz_deli, other_deli)
      puts "Welcome, #{other_deli}. You are #{katz_deli.length} in line"
      
-     #puts take_a_number(katz_deli, name)
+     
 end
 
