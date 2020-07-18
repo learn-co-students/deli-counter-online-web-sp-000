bb9cff1d391b868db50cd5831f253a0737bab84d
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-18 22:57:21 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+
+katz_deli = []
+
+def take_a_number(katz_deli)
+  puts "Welcome to Katz's Deli.  What is your name?"
+  gets name 
+  katz_deli[] << name 
+  placement = katz_deli.find.index("name") + 1
+  puts "Welcome, #{name}.  You are number #{placement} in line."
+end
+
+def line(katz_deli)
+  index = 0
+  while index < katz_deli.length 
+    name = katz_deli[index]
+    puts "The line is currently: #{index + 1}. #{name} "
+    index += 1 
+  end
+    puts "The line is currently empty."
+end
+
+def now_serving(katz_deli)
+  serving = katz_deli.shift
+  while katz_deli.any?
+    puts "Currently serving: #{serving}."
+  end
+end
