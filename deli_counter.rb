51cabdd7a44347b6f6d2edbcff8eb0b31d751ae7
Jean-Victor Brun <jeanvbrun@icloud.com>
2020-07-28 01:08:28 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,11 +11,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty." 
   else
-    place = 1 
-    katz_deli.each do |n| 
-      puts "The line is currently: #{place}. #{n} "
-      place = place + 1 
-    end
+      katz_deli.each_with_index {|val, index| puts "The line is currently: #{index + 1}. #{val}" }
   end
 end
 
