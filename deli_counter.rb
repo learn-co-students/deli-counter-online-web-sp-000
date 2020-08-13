00f532f85cd7c6b313b172076e1b0201f553c076
shacoca <shacoca@yahoo.com>
2020-08-13 13:52:08 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+katz_deli = []
+
+def line(queue)
+    if queue = []
+        message = "The line is currently empty."
+    else
+
+    end
+end
+
+def take_a_number(queue, name)
+
+end
