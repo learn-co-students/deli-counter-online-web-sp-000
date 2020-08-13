e3a4fddc6af964f8d1d865e37bf0afe8d1415bb9
shacoca <shacoca@yahoo.com>
2020-08-13 13:54:11 -0700

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,5 +9,12 @@ def line(queue)
 end
 
 def take_a_number(queue, name)
+    # The method should call out (`puts`) the person's name along with their position in line.
+    
+end
+
+def now_serving()
 
 end
+
+# Build the `now_serving` method which should call out (i.e. `puts`) the next person in line and then remove them from the front. If there is nobody in line, it should call out (`puts`) that `"There is nobody waiting to be served!"`.
