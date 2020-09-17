0eb2c0bab995bba8bf93cfc21da6ad4ec0569775
fut0k0 <cccabell@icloud.com>
2020-09-17 15:36:40 -0700

    Passing lab

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(array)
+	if array.size == 0
+		puts "The line is currently empty."
+	else
+		puts "The line is currently:#{array.map.with_index {|name, i| " #{i + 1}. #{name}"} .join}"
+	end
+end
+
+def take_a_number(array, name)
+	puts "Welcome, #{name}. You are number #{array.push(name).size} in line."
+end
+
+def now_serving(array)
+	if array.size == 0
+		puts "There is nobody waiting to be served!"
+	else
+		puts "Currently serving #{array.shift}."
+	end
+end
