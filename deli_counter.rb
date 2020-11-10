189325c2dd0a3d01c6d013c293d1c2ad82cb9898
Ben Altman <altmanguitar@gmail.com>
2020-11-10 14:18:53 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
-def line(in_line)
-  if in_line.length >= 1
-    puts in_line
+katz_deli = []
+
+def line(array)
+  customers = []
+  if array.length >= 1
+    array.each_with_index do |value, index|
+    puts "The line is currently: #{index}. #{name}"
   else
     puts "The line is currently empty."
   end
