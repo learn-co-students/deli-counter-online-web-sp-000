7a920b0c522cd434ef504cd9798bf3721442c77e
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-25 01:24:37 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,7 @@
   else
      string = "The line is currently:"
      deli.map.with_index(1) do |names, index| 
-       string += " #{index}.#{names}"
+       string += " #{index}. #{names}"
         end 
        puts string 
      end
