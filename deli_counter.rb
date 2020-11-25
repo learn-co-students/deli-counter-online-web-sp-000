7188fec19de8efa5d52dce0ef3044f45569d952f
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-25 00:38:35 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,11 @@ def line(deli)
     puts "The line is currently empty."
   end 
   if deli == other_deli
-  deli.map.with_index(1){|names, index| "#{index}.#{names}" puts "The line is currently: #{index}.#{names}"
+  deli.map.with_index(1) do |names, index| 
+    puts "The line is currently: #{index}.#{names}"
      end 
   if deli == another_deli
-    deli.map.with_index(1){|names, index| "#{index}.#{names}" puts "The line is currently: #{index}.#{names}"
+    deli.map.with_index(1){|names, index| 
+    puts "The line is currently: #{index}.#{names}"
   end
 end 
