faf8d862fc8f501d61031821cf6705265ec7c8dc
HELEN HARMAN <hlharman@bellsouth.net>
2020-11-26 05:05:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,3 +1,4 @@
+ require "pry" 
  def line(deli)
   if deli.size == 0 
     puts "The line is currently empty."
@@ -11,9 +12,18 @@
 end 
 
 
- def take_a_number(katz_deli, name = "Ada")
-katz_deli << name 
+def take_a_number(katz_deli, name = "Ada")
+  katz_deli << name 
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end 
  
-  
+ 
+ 
+def now_serving(line) 
+  if line.size == 0 
+    puts "There is nobody waiting to be served!"
+  else 
+    puts "Currently serving #{line.first}." 
+  end 
+    line.shift  
+end  
