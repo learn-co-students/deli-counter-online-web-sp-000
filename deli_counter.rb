a31b3c3a8285652f4333818b2cf40836dbff16ac
Cody Alvarez <cody_alvarez19@outlook.com>
2021-02-27 18:30:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,9 +15,19 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+# def now_serving(array)
+#   if array.empty?
+#     puts "There is nobody waiting to be served!"
+#   else
+#     puts "Currently serving #{array[0]}."
+#     array.shift
+#   end
+# end
+
 def now_serving(array)
-  if array.empty?
-    puts "There is nobody waiting to be served!"
+  case now_serving
+  when 0
+    puts "There is nobody waiting to be served!."
   else
     puts "Currently serving #{array[0]}."
     array.shift
