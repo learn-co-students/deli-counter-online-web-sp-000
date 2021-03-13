fdd4d2aedbb66b3e36800eb2c4ca3644cf25c09b
Harrison Price <hprice518@gmail.com>
2021-03-13 21:00:06 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+
+
+def line(katz_deli) 
+  if katz_deli = []
+    puts "The line is currently empty."
+  else
+    puts "The line is currently #{other_deli}."
+  end
+end
+
+
+
+def take_a_number(katz_deli, name)
+  
+end
+
+
+
+def now_serving
+
+end
