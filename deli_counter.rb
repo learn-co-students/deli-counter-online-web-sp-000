b8cf35a12be2ab9857b244882ac0e93397e5b55e
Harrison Price <hprice518@gmail.com>
2021-03-14 01:33:58 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,11 @@
 require 'pry'
 
 def line(new_guest) 
-  if new_guest_array.length = []
+  if new_guest_array == []
     puts "The line is currently empty."
   else
-    new_guest.each.with_index(1) do |name, index|
-      new_guest_array << "#{index}. #{name} "
+    new_guest.each do |name, index|
+      new_guest_array.push ("#{index}. #{name} ")
     end
       puts "The line is currently: #{new_guest_array.join(" ")}."
   end
