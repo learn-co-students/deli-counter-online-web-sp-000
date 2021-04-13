2360931dc5fbc006c9b864cb0e5a9b0027038364
Drew Keat <thedrewkeat@gmail.com>
2021-04-13 11:05:57 -0600

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+require 'pry'
+def line(array)
+    message = "The line is currently"
+    if array.length == 0
+        puts "#{message} empty."
+    else
+        line_string = ""
+        array.each_with_index do |person,i|
+            place = i + 1
+            line_string += "#{place}. #{person} "
+        end
+        puts "#{message}: #{line_string.strip}"
+    end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{array.last}. You are number #{array.size} in line."
+end
+
+def now_serving(array)
+    array.size < 1 ? (puts "There is nobody waiting to be served!") : (puts "Currently serving #{array.shift}.")
+end
