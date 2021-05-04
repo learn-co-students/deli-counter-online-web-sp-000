fcb9e547ab7df122a785e7f435dbdbdd07dbd576
Siobhan The Destroyer <siobhancreagan@gmail.com>
2021-05-04 03:54:49 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    line = katz_deli.map.with_index {|name, index| [index + 1, name].join(". ")}.join(" ")
+    puts "The line is currently: ".concat(line)
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  spot = katz_deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{spot} in line."
+end
+
+def now_serving(katz_deli)
+  if katz_deli == []
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+  end
+end
