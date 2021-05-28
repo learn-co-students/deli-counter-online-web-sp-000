cdcdbef472fad66cf2b7ca7078fae5030200f17a
Ian King <kickthechair@gmail.com>
2021-05-28 15:04:30 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 
-katz_deli = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
+# = ["Amanda", "Annette", "Ruchi", "Jason", "Logan", "Spencer", "Avi", "Joe", "Rachel", "Lindsey"]
 
 def line(queue_array)
   if queue_array == []
@@ -21,19 +21,28 @@ end
 
 
 
-queue_array = []
-name = "Daisey"
+#queue_array = ["a"]
+#name = "ada"
 
-def take_a_number(queue_array, customer_name)
+def take_a_numberz(queue_array, customer_name)
   queue_array.push(customer_name)
-  line = "Welcome #{customer_name}. You are number" 
+  line = "Welcome, #{customer_name}. You are number" 
   position = queue_array.index(customer_name)
   end_of_line = "in line."
-  puts "#{line} #{position} #{end_of_line}"
+  puts "#{line} #{position + 1} #{end_of_line}"
+end
+
+def take_a_numberz(queue_array, customer_name)
+  queue_array.push(customer_name)
+  line = "Welcome, #{customer_name}. You are number" 
+  position = queue_array.index(customer_name)
+  end_of_line = "in line."
+  puts "#{line} #{position + 1} #{end_of_line}"
 end
 
 
-take_a_number(queue_array, name)
+
+#take_a_number(queue_array, name)
 
 
 
@@ -41,10 +50,3 @@ take_a_number(queue_array, name)
 
 
 
-#should puts the person's name along with their position in line
-
-#queue_array = name of customers array
-#customer_name = string containing the name of the person joining the end of the line
-
-#expect to see:
-#"Welcome, Ada. You are number 1 in line."
