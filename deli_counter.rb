1cb543b8c2ec313d26f0e44f1fcb0e116164be07
Ian King <kickthechair@gmail.com>
2021-05-28 13:17:22 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,45 +22,5 @@ line(katz_deli)
 #we need the persons name
 #output everything to one line
 
-
+#what I expect to see in output:
 #"The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. Avi 8. Joe 9. Rachel 10. Lindsey
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#def take_a_number(queue_array, customer_name)
-#
-#end
-
-
-
-#The method should call out (puts) the person's name along with their position in line. Top-Tip: Remember that people like to count from 1, not from 0 ("zero") like computers
