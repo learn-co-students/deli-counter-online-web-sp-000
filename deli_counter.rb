8b04e00e76611480a7265c35c87a6a6161060bbf
Ian King <kickthechair@gmail.com>
2021-05-28 13:46:44 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(queue_array)
     puts "The line is currently empty."
   else
     opening_line =  "The line is currently: "
-    queue_array.each_index { |index| index + 1. queue_array[index] " }
+    queue_array.each_index { |index| index + 1. queue_array[index] }
   end
 end
 
